d78b888d44747fe4d1a77991269f55eed7b06910
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-11-24 19:26:22 -0300

    creating a function to return the Expanded widget customized to each section

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,54 +7,42 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    void playSound(){
-      print('Play sound');
-      final player = AudioPlayer();
-      await player.setSource(AssetSource('assets/note1.wav'));
+    void playSound (int fileNumber) {
+      final player = AudioCache();
+      player.play('note$fileNumber.wav');
+    }
+
+    Expanded sectionComponent(Color colorProperty, int soundNumber){
+      return Expanded(
+        flex: 1,
+        child: TextButton(
+          style: TextButton.styleFrom(
+            padding: EdgeInsets.zero,
+          ),
+          child: Container(
+            color: colorProperty,
+          ),
+          onPressed: (){
+            playSound(soundNumber);
+          },
+        ),
+      );
     }
 
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: TextButton(
-                  child: Container(
-                    color: Colors.red,
-                    ),
-                  onPressed: (){
-                    playSound();
-                    },
-                  ),
-                ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  color: Colors.yellow,
-                )
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  color: Colors.blue,
-                )
-              )
-            ]
-          ),
+        body: Column(
+          children: <Widget>[
+            sectionComponent(Colors.red, 1),
+            sectionComponent(Colors.blue, 2),
+            sectionComponent(Colors.green, 3),
+            sectionComponent(Colors.yellow, 4),
+            sectionComponent(Colors.brown, 5),
+            sectionComponent(Colors.amber, 6),
+            sectionComponent(Colors.lightBlueAccent, 7),
+          ]
         ),
       ),
     );
   }
 }
-
-// Expanded(
-// child: TextButton(
-// child: Text('second'),
-// style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
-// onPressed: (){
-// playSound();
-// },
-// )
-// )
